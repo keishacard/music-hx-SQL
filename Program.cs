169a0d64a91e321fd0fb3e7b72b63e3639c57432
Keisha <49162665+keishacard@users.noreply.github.com>
2019-08-02 16:06:47 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string foo = "foo";
+            int bar = 10;
+            string boop = "Josh Boop";
+            int baz = 15;
+        }
+    }
+}
